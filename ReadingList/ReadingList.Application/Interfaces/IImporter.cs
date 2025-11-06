d3d7e09f62172dfd5ed472597f6677f7bbe4f736
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-06 16:32:25 +0200

    created interface for importing books from a csv

---
+++ ReadingList/ReadingList.Application/Interfaces/IImporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingList.Application.Interfaces
+{
+    public interface IImporter<T>
+    {
+        public Task<IEnumerable<T>> ImportFromFileAsync(params string[] filePaths);
+    }
+}
