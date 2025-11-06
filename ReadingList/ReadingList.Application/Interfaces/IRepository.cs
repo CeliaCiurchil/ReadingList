741b58cb1b166eaf7c38702d0fa8cfba80fd75dd
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-06 12:01:49 +0200

    created interface for generic repository in application layer

---
+++ ReadingList/ReadingList.Application/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ReadingList.Application.Interfaces;
+
+public interface IRepository<T>
+{
+    IEnumerable<T> GetAll();
+    T GetById(int id);
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
